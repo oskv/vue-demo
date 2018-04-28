af27d0a9736efc638bf0e023913b9019c08194d1
Alexandr Skakovskyj <alex.skakovsky@gmail.com>
2018-04-28 22:16:40 +0300

    set up jest

---
+++ test/App.spec.js
@@ -0,0 +1,8 @@
+import { shallow } from '@vue/test-utils'
+import List from '@/components/App.vue'
+
+describe('List.vue', () => {
+  it('renders li for each item in props.items', () => {
+    expect('aa').toEqual('aa');
+  })
+})
